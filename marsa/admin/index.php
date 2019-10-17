d6451dc33c0e352681ed5d62fdbda9a4bbf1f975
mks110 <setiawanmarsa388@gmail.com>
2019-10-17 13:35:30 +0700

    Tampilan multi login + tampilan web

--- marsa/admin/index.php
+++ marsa/admin/index.php
@@ -1,28 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome to admin page</title>
+    <title>form login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body>
-	<h2>Halaman Admin</h2>
-	
-	<br/>
- 
-	
-	<?php 
-	session_start();
-	if($_SESSION['status']!="login"){
-		header("location:../index.php?pesan=belum_login");
-	}
-	?>
- <h3>BONJOUURR!!</h3>
-	<h4>ANDA TELAH LOGIN !! <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
- 
-	<br/>
-	<br/>
- 
-	<a href="logout.php">LOGOUT</a>
- 
- 
+    <h1>Selamat Datang di Login Page</h1>
+    <?php
+    if(isset($_GET['pesan'])){
+        if($_GET['pesan']=="gagal"){
+        echo "<div class='alert'>Username password Salah!</div>";
+    }
+}
+?>
+<div class="kotak_login">
+<p class="tulisan_login">Silahkan Login</p>
+
+<form action="cek_login.php" method="post">
+
+<label>Username</label>
+<input type="text" name="username" class="form_login" placeholder="username.."required="required">
+
+<label>Password</label>
+<input type="password" name="password" class="form_login" placeholder="password.."equired="required">
+
+<input type="submit" class="tombol_login" value="login" >
+<br/>
+<br/>
+<center>
+<a class="link" href="">kembali</a>
+</center>
+</form>
+</div>
 </body>
 </html>
