6ca71605efe3d738be70540a047b326dfd46800e
brianv4 <55471120+brianv4@users.noreply.github.com>
2019-11-07 15:37:24 +0700

    pendaftaran peserta

---
+++ Project/pendaftaran-online-master/daftar-pelatihan/proses-daftar-pelatihan.php
@@ -0,0 +1,29 @@
+<?php 
+if(isset($_POST['daftar'])){
+	include 'db.php';
+	if(!is_numeric($_POST['telp'])){
+		header("location:form-daftar-pelatihan.php?err1");
+	}else{
+		$daftar = mysqli_query($koneksi, "INSERT INTO tb_daftarpelatihan VALUES(
+			NULL,
+			'".$_POST['nama']."',
+			'".$_POST['nik']."',
+			'".$_POST['tmp_lhr']."',
+			'".$_POST['tgl_lhr']."',
+			'".$_POST['jk']."',
+			'".$_POST['telp']."',
+			'".$_POST['email']."',
+			'".$_POST['pass']."',
+			'".$_POST['alamat']."',
+			'".$_POST['kode_pos']."',
+			'".$_POST['provinsi']."',
+			'".$_POST['pendidikan']."')");
+			
+		if($daftar){
+			echo 'Pendaftaran Berhasil';
+		}else{
+			echo 'Error'.mysqli_error($koneksi);
+		}
+	}
+}
+?>
