0000d78f60db1f3f0f4e5b9d91cba33dee4a862c
mks110 <setiawanmarsa388@gmail.com>
2019-11-07 15:18:12 +0700

    ini input materi (bisa)

--- adiirawan/pribadi/inputkursus.php
+++ adiirawan/pribadi/inputkursus.php
@@ -1,50 +1,103 @@
+<?php
+include("koneksi.php");
+include("func.php");
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Membuat CRUD Dengan PHP Dan MySQL - Menampilkan data dari database</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Manajemen</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+	
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<?php
+	error_reporting(0)
+	?>
 </head>
 <body>
-	<div class="judul">		
-		<h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
-		<h2>Menampilkan data dari database</h2>
-		<h3>www.malasngoding.com</h3>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand visible-xs-block visible-sm-block" href="#">Manajemen User</a>
+				<a class="navbar-brand hidden-xs hidden-sm" href="#">Manajemen User</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Beranda</a></li>
+					<li class="active"><a href="add.php">Tambah Data</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Manajemen User &raquo; Tambah Data User</h2>
+			<hr />
+			
+			<?php
+			if(isset($_POST['add'])){
+				$id		= aman($_POST['id']);
+				$nama	= aman($_POST['nama']);
+				
+				
+				//tinggal masalah database NIK dijadikan BIG INT
+				$cek = mysqli_query($koneksi, "SELECT * FROM `materi_kursus` WHERE ID_MATERIKURSUS='$id'");
+				$insert = mysqli_query($koneksi, "INSERT INTO `materi_kursus`(`ID_MATERIKURSUS`, `NAMA_KURSUS`) VALUES ('$id','$nama')") or die(mysqli_error($koneksi));
+						if($insert){
+							echo '<div class="alert alert-success">Pendaftaran berhasil dilakukan.</div>';
+						}else{
+							echo '<div class="alert alert-danger">Pendaftaran gagal dilakukan, silahkan coba lagi.</div>';
+                        }
+            }
+			?>
+			
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">ID</label>
+					<div class="col-sm-2">
+						<input type="text" name="id" class="form-control" placeholder="NIK" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nama Materi</label>
+					<div class="col-sm-4">
+						<input type="text" name="nama" class="form-control" placeholder="nama" required>
+                    </div>
+                    </div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="add" class="btn btn-primary" value="TAMBAH">
+						<a href="index.php" class="btn btn-warning">BATAL</a>
+					</div>
+				</div>
+			</form>
+		</div>
 	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
 	
-	<br/>
-
-	<a href="inputkursus.php">Lihat Semua Data</a>
-
-    
-    <br/>
-    <?php 
-if(isset($_GET['pesan'])){
-	$pesan = $_GET['pesan'];
-	if($pesan == "input"){
-		echo "Data berhasil di input.";
-	}else if($pesan == "update"){
-		echo "Data berhasil di update.";
-	}else if($pesan == "hapus"){
-		echo "Data berhasil di hapus.";
-	}
-}
-?>
-	<h3>Input data baru</h3>
-	<form action="simpan.php" method="post">		
-		<table>
-			<tr>
-				<td>ID</td>
-				<td><input type="text" name="id"></td>					
-			</tr>	
-			<tr>
-				<td>NAMA</td>
-				<td><input type="text" name="nama"></td>					
-			</tr>	
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Simpan"></td>					
-			</tr>				
-		</table>
-	</form>
 </body>
 </html>
--- adiirawan/pribadi/simpan.php
+++ adiirawan/pribadi/simpan.php
@@ -4,7 +4,7 @@ $id = $_POST['id'];
 $nama = $_POST['nama'];
 
 
-mysql_query("INSERT INTO `materi_kursus`(`ID_MATERIKURSUS`, `NAMA_KURSUS`) VALUES ($id,$nama])");
+mysql_query("INSERT INTO `materi_kursus`(`ID_MATERIKURSUS`, `NAMA_KURSUS`) VALUES '$id','$nama'");
 
 header("location:inputkursus.php?pesan=input");
 ?>
